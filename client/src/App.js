17cb24505cc100d4de7d6567faf8a0f477fd3d08
Lee Anderson <ecllips07@gmail.com>
2021-09-22 14:45:44 -0600

    init

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Container } from "semantic-ui-react";
+import { Route, Switch } from "react-router";
+import Home from "./pages/Home";
+import Things from "./pages/Things";
+import ComponentDemo from "./pages/CompentDemo";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/things" component={Things} />
+          <Route exact path="/components" component={ComponentDemo} />
+          <Route component={() => <p>react router 404 path not found</p>} />
+        </Switch>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
+
+class Navbar extends React.Component {
+  render() {
+    const { location } = this.props;
+    return (
+      <Menu>
+        <Link to="/">
+          <Menu.Item active={location.pathname == "/"}>Home</Menu.Item>
+        </Link>
+        <Link to="/things">
+          <Menu.Item active={location.pathname == "/things"}>Things</Menu.Item>
+        </Link>
+        <Link to="/components">
+          <Menu.Item active={location.pathname == "/components"}>
+            Components
+          </Menu.Item>
+        </Link>
+      </Menu>
+    );
+  }
+}
+
+export default withRouter(Navbar);
